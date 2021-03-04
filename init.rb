9777fe9b4728fea1d53890522dbca5927fcf13b0
川﨑　澄明 <kawasaki@d-seeds.com>
2021-03-04 19:22:16 +0900

    generate plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :issues_catalog do
+  name 'Issues Catalog plugin'
+  author 'ds-kawasaki'
+  description 'issues catalog plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/ds-kawasaki/issues_catalog'
+  author_url 'https://github.com/ds-kawasaki/issues_catalog'
+end
