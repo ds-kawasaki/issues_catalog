f5234a645df594b738442216f29bfc5e8be65b93
川﨑　澄明 <kawasaki@d-seeds.com>
2021-05-28 12:43:39 +0000

    プラグイン名モジュールで囲みました

--- lib/issues_catalog/patches/issues_catalog_projects_helper_patch.rb
+++ lib/issues_catalog/patches/issues_catalog_projects_helper_patch.rb
@@ -1,17 +1,22 @@
 require_dependency 'projects_helper'
 
-module ProjectsHelperMethodsIssuesCatalog
-  def project_settings_tabs
-    tabs = super
-
-    action = { name: 'issues_catalog',
-      action: :show,
-      partial: 'issues_catalog_settings/show',
-      label: :issues_catalog }
-    tabs << action if @project.module_enabled?(:issues_catalog)
-
-    tabs
+module IssuesCatalog
+  module Patches
+    module ProjectsHelperMethodsIssuesCatalog
+      def project_settings_tabs
+        tabs = super
+    
+        action = { name: 'issues_catalog',
+          action: :show,
+          partial: 'issues_catalog_settings/show',
+          label: :issues_catalog }
+        tabs << action if @project.module_enabled?(:issues_catalog)
+    
+        tabs
+      end
+    end
   end
 end
 
-ProjectsHelper.prepend(ProjectsHelperMethodsIssuesCatalog)
+
+ProjectsHelper.prepend(IssuesCatalog::Patches::ProjectsHelperMethodsIssuesCatalog)
