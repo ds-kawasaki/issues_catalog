5294a6ed9f063b9b21fedf333241c626977d9a27
川﨑　澄明 <kawasaki@d-seeds.com>
2021-06-18 06:20:11 +0000

    カタログのチケット項目表示を独自関数化

--- app/helpers/issues_catalog_helper.rb
+++ app/helpers/issues_catalog_helper.rb
@@ -27,13 +27,13 @@ module IssuesCatalogHelper
                 div_tr << content_tag(:td, check_box_tag("ids[]", issue.id, false, id: nil), class: 'checkbox hide-when-print')
                 div_tr << "\n"
                 @query.inline_columns.each do |column|
-                  div_tr << content_tag(:td, column_content(column, issue), class: column.css_classes)
+                  div_tr << content_tag(:td, catalog_column_content(column, issue), class: column.css_classes)
                   div_tr << "\n"
                 end
                 div_tr << content_tag(:td, link_to_context_menu, class: 'buttons')
               end
               @query.block_columns.each do |column|
-                if (text = column_content(column, issue)) && text.present?
+                if (text = catalog_column_content(column, issue)) && text.present?
                   div_tbody << content_tag_push(:tr, class: current_cycle) do |div_tr|
                     td_class = column.css_classes + ' block_column'
                     td_colspan = @query.inline_columns.size + 2
@@ -53,6 +53,51 @@ module IssuesCatalogHelper
     end
     return raw(html_text)
   end
+
+  def catalog_column_content(column, item)
+    value = column.value_object(item)
+    if column.name == :tags
+      column.value(item).collect{ |t| render_catalog_link_tag(t) }.join(', ').html_safe
+    elsif value.is_a?(Array)
+      values = value.collect {|v| catalog_column_value(column, item, v)}.compact
+      safe_join(values, ', ')
+    else
+      catalog_column_value(column, item, value)
+    end
+  end
+
+  def catalog_column_value(column, item, value)
+    case column.name
+    when :id
+      link_to value, issue_path(item)
+    when :subject
+      link_to value, issue_path(item)
+    when :parent
+      value ? (value.visible? ? link_to_issue(value, :subject => false) : "##{value.id}") : ''
+    when :description
+      item.description? ? content_tag('div', textilizable(item, :description), :class => "wiki") : ''
+    when :last_notes
+      item.last_notes.present? ? content_tag('div', textilizable(item, :last_notes), :class => "wiki") : ''
+    when :done_ratio
+      progress_bar(value)
+    when :relations
+      content_tag(
+        'span',
+        value.to_s(item) {|other| link_to_issue(other, :subject => false, :tracker => false)}.html_safe,
+        :class => value.css_classes_for(item))
+    when :hours, :estimated_hours
+      format_hours(value)
+    when :spent_hours
+      link_to_if(value > 0, format_hours(value), project_time_entries_path(item.project, :issue_id => "#{item.id}"))
+    when :total_spent_hours
+      link_to_if(value > 0, format_hours(value), project_time_entries_path(item.project, :issue_id => "~#{item.id}"))
+    when :attachments
+      value.to_a.map {|a| format_object(a)}.join(" ").html_safe
+    else
+      format_object(value)
+    end
+  end
+
 
   def render_catalog_issues
     html_text = ''
