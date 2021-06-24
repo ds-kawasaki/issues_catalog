ffab95792eab01291940d4320233063c1b1fdf1d
川﨑　澄明 <kawasaki@d-seeds.com>
2021-06-24 07:50:05 +0000

    カテゴリタブ左右スクロールをリファクタリング 一度の移動を最大で表示幅の半分に

--- assets/javascripts/issues_catalog.js
+++ assets/javascripts/issues_catalog.js
@@ -31,7 +31,7 @@ $(function () {
 
   // メインカテゴリタブ切替クリック時 
   const tags = $('.category-tab');
-  $('.category-tab').on('click', function () {
+  tags.on('click', function () {
     $('.active-tab').removeClass('active-tab');
     $(this).addClass('active-tab');
     const index = tags.index(this);
@@ -65,27 +65,56 @@ $(function () {
     $('.category-content').removeClass('show-content').eq(index).addClass('show-content');
   };
 
+
+  const scrollHorizontal = (area, direction) => {
+    const element = $(area);
+    const areaWidth = element.outerWidth();
+    const scrollWidth = element.get(0).scrollWidth;
+    const movableWidth = scrollWidth - areaWidth;
+    const oneScrollValue = Math.min(movableWidth, areaWidth / 2);
+    const current = element.get(0).scrollLeft;
+    let newValue, isLimit;
+    if (direction < 0) {
+      newValue = Math.max(current - oneScrollValue, 0);
+      isLimit = newValue === 0;
+    } else {
+      newValue = Math.min(current + oneScrollValue, movableWidth);
+      isLimit = newValue === movableWidth;
+    }
+    element.animate({
+      scrollLeft: newValue
+    });
+    return isLimit;
+  };
+  //  メインカテゴリタブの左右スクロールボタンクリック時
+  $('#tabs-scrl-l-btn').on('click', function () {
+    if (scrollHorizontal('.tabs-area', -1)) {
+      $(this).hide();
+    }
+    $('#tabs-scrl-r-btn').show();
+  });
+  $('#tabs-scrl-r-btn').on('click', function () {
+    if (scrollHorizontal('.tabs-area', 1)) {
+      $(this).hide();
+    }
+    $('#tabs-scrl-l-btn').show();
+  });
+
   //  メインカテゴリタブの左右スクロール可否調整
   const resizedCatalogTab = () => {
     const tabsArea = $('.tabs-area');
     const areaWidth = tabsArea.outerWidth();
-    const tabsWidth = tabsArea.get(0).scrollWidth;
-    const scrollWidth = parseInt(tabsWidth - areaWidth);
-    if (scrollWidth > 0) {
+    const scrollWidth = tabsArea.get(0).scrollWidth;
+    const movableWidth = scrollWidth - areaWidth;
+    if (movableWidth > 0) {
       $('.tabs-scrl-btn').show();
-      // const scrollPos = tabsArea.get(0).scrollLeft;
-      // if (scrollPos > 0) {
-      //   $('#tabs-scrl-r-btn').hide();
-      // }
-      // else {
-      //   $('#tabs-scrl-l-btn').hide();
-      // }
     } else {
       $('.tabs-scrl-btn').hide();
     }
   };
 
-  const resize = () => {
+  //  読み込み時とウィンドウリサイズ時のコールバック登録
+  const setReseizeCallback = () => {
     let timeoutID = 0;
     const delay = 250;
     window.addEventListener('resize', () => {
@@ -96,28 +125,10 @@ $(function () {
     });
     setTimeout(resizedCatalogTab, 0); //  初回
   };
-  resize();
 
-  $('#tabs-scrl-l-btn').on('click', function() {
-    $('.tabs-area').animate({
-      scrollLeft: 0
-    });
-    $(this).hide();
-    $('#tabs-scrl-r-btn').show();
-  });
-  $('#tabs-scrl-r-btn').on('click', function() {
-    const tabsArea = $('.tabs-area');
-    const areaWidth = tabsArea.outerWidth();
-    const tabsWidth = tabsArea.get(0).scrollWidth;
-    const scrollWidth = parseInt(tabsWidth - areaWidth);
-    tabsArea.animate({
-      scrollLeft: scrollWidth
-    });
-    $(this).hide();
-    $('#tabs-scrl-l-btn').show();
-  });
 
   setCatalogTabOnLoad();
+  setReseizeCallback();
 
 });
 
