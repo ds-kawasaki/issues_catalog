562ae073464b595296bddf270541966c499026cc
川﨑　澄明 <kawasaki@d-seeds.com>
2021-08-31 10:04:48 +0000

    カタログ設定画面のすべてにチェックOnOff実装

--- assets/javascripts/issues_catalog_settings.js
+++ assets/javascripts/issues_catalog_settings.js
@@ -1,13 +1,14 @@
 // jQuery用DOM準備完了時 document ready
 $(function () {
+  //  一括タグカテゴリの「一括追加」「一括削除」ボタン押下時
   $('#form-bulk-edit-tag-categories button').on('click', function() {
     const selectCategories = $('#select-catalog-tag-categories').val();
     if (!selectCategories.length) { return; }
     // console.log(selectCategories);
     const form = $(this).parents('form');
-    form.find('[name="ids[]"]').remove();
+    form.find('[name=ids\\[\\]]').remove();
     let checkIdes = false;
-    $('input[name="ids[]"]:checked').each(function() {
+    $('input[name=ids\\[\\]]:checked').each(function() {
       $('<input>').attr({'type':'hidden', 'name':'ids[]'}).val($(this).val()).appendTo(form);
       checkIdes = true;
     });
@@ -15,5 +16,11 @@ $(function () {
       form.find('[name=operate]').val($(this).val());
       form.submit();
     }
+  });
+
+  //  すべてにチェックつける・はずす
+  $('input[type=checkbox].toggle-selection').on('change', function() {
+    const checked = $(this).prop('checked');
+    $(this).parents('table').find('input[name=ids\\[\\]]').prop('checked', checked);
   });
 });
