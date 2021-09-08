2f34eaaa56e8e8848ffdaf2a282314859f1e6ee8
川﨑　澄明 <kawasaki@d-seeds.com>
2021-09-08 10:23:13 +0000

    標準インポートにタグ追加

---
+++ lib/issues_catalog/patches/issue_import_patch.rb
@@ -0,0 +1,30 @@
+module IssuesCatalog
+  module Patches
+    module IssueImportPatch
+      def self.included(base)
+        base.send(:include, InstanceMethods)
+        base.class_eval do
+          alias_method :extend_object_without_tags, :extend_object
+          alias_method :extend_object, :extend_object_with_tags
+        end
+      end
+
+      module InstanceMethods
+        def extend_object_with_tags(row, item, issue)
+          extend_object_without_tags(row, item, issue)
+
+          if tags = row_value(row, 'tags')
+            issue.tag_list = tags
+            issue.save_tags
+          end
+
+          issue
+        end
+      end
+    end
+  end
+end
+
+base = IssueImport
+patch = IssuesCatalog::Patches::IssueImportPatch
+base.send(:include, patch) unless base.included_modules.include?(patch)
