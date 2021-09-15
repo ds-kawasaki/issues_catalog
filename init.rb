3c50c4319c28ce9f40a65202b27094c9df62318d
川﨑　澄明 <kawasaki@d-seeds.com>
2021-09-15 19:04:58 +0900

    バージョン番号0.6.2に

--- init.rb
+++ init.rb
@@ -12,7 +12,7 @@ Redmine::Plugin.register :issues_catalog do
   name 'Issues Catalog plugin'
   author 'ds-kawasaki'
   description 'issues catalog plugin for Redmine'
-  version '0.6.1'
+  version '0.6.2'
   url 'https://github.com/ds-kawasaki/issues_catalog'
   author_url 'https://github.com/ds-kawasaki'
 
