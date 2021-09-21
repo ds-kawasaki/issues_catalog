abb8de80862dcaf3e395b9f7ac95df39337a9925
川﨑　澄明 <kawasaki@d-seeds.com>
2021-09-21 02:48:19 +0000

    issues_catalog_setting.jsリファクタリング コード整理

--- assets/javascripts/issues_catalog_settings.js
+++ assets/javascripts/issues_catalog_settings.js
@@ -41,6 +41,7 @@ $(function () {
     for (const option of bulkSelect?.options) {
       if (option.text === oldName) { option.text = newName; }
     }
+    //  Select2の中身を更新（上手く更新できていないけど）
     const childRecursive = (elem) => {
       for (const child of elem.children) { childRecursive(child); }
       if (elem.innerText.includes(oldName)) {
@@ -57,72 +58,54 @@ $(function () {
       childRecursive(selection);
     }
   };
-  const editedCategoryItem = (event) => {
-    const target = event.target;
-    target.innerText = target.innerText.replace(/[\r\n]/g, '').trim();  //  改行・冒頭末尾余白削除 
-    const oldValue = target.getAttribute('data-value');
-    if (oldValue === target.innerText) return true;
-    if (target.innerText.length === 0) {
-      target.innerText = oldValue;
-      return true;
-    }
-    event.preventDefault();
+
+  //  タグカテゴリの項目編集
+  const editedCategoryItem = (target, value, oldValue) => {
     const category_id = target.parentNode?.id?.slice(21); // 21='catalog_tag_category-'.length
     const column = target.classList.item(0);
-    const value = target.innerText || '__none__';
     console.log(`category ${category_id} : ${column} : ${value}`);
     if (column === 'name') { updateCategoryName(category_id, oldValue, value); }  // タグカテゴリ名称変更を各所の表示反映
-    return false;
-  };
-  //  タグカテゴリの項目編集
-  for (const edit of document.querySelectorAll('.edit-category')) {
-    edit.contentEditable = true;
-    edit.setAttribute('data-value', edit.innerText);
-    edit.addEventListener('keydown', function (event) {
-      if (event.key === 'Enter') {
-        event.preventDefault();
-      }
-    });
-    edit.addEventListener('focusout', function (event) {
-      editedCategoryItem(event);
-    });
-    edit.addEventListener('focusin', function (event) {
-      event.target.setAttribute('data-value', event.target.innerText);
-    });
-  }
-
-  const editedTagItem = (event) => {
-    const target = event.target;
-    target.innerText = target.innerText.replace(/[\r\n]/g, '').trim();  //  改行・冒頭末尾余白削除 
-    const oldValue = target.getAttribute('data-value');
-    if (oldValue === target.innerText) return true;
-    if (target.innerText.length === 0) {
-      target.innerText = oldValue;
-      return true;
-    }
-    event.preventDefault();
-    const tag_id = target.parentNode?.id?.slice(4); // 4='tag-'.length
-    const column = target.classList.item(0);
-    const value = target.innerText || '__none__';
-    console.log(`tag ${tag_id} : ${column} : ${value}`);
-    return false;
   };
   //  タグの項目編集
-  for (const edit of document.querySelectorAll('.edit-tag')) {
-    edit.contentEditable = true;
-    edit.setAttribute('data-value', edit.innerText);
-    edit.addEventListener('keydown', function (event) {
-      if (event.key === 'Enter') {
-        event.preventDefault();
+  const editedTagItem = (target, value, oldValue) => {
+    const tag_id = target.parentNode?.id?.slice(4); // 4='tag-'.length
+    const column = target.classList.item(0);
+    console.log(`tag ${tag_id} : ${column} : ${value}`);
+  };
+  const setupEdit = (className, editedCallback) => {
+    const editedItem = (event, editedCallback) => {
+      const target = event.target;
+      target.innerText = target.innerText.replace(/[\r\n]/g, '').trim();  //  改行・冒頭末尾余白削除 
+      const oldValue = target.getAttribute('data-value');
+      if (oldValue === target.innerText) return true;
+      if (target.innerText.length === 0) {
+        target.innerText = oldValue;
+        return true;
       }
-    });
-    edit.addEventListener('focusout', function (event) {
-      editedTagItem(event);
-    });
-    edit.addEventListener('focusin', function (event) {
-      this.setAttribute('data-value', this.innerText);
-    });
-  }
+      event.preventDefault();
+      if (editedCallback) {
+        editedCallback(target, target.innerText || '__none__', oldValue);
+      }
+      return false;
+    };
+    for (const edit of document.querySelectorAll(className)) {
+      edit.contentEditable = true;
+      edit.setAttribute('data-value', edit.innerText);
+      edit.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+          event.preventDefault();
+        }
+      });
+      edit.addEventListener('focusout', function (event) {
+        editedItem(event, editedCallback);
+      });
+      edit.addEventListener('focusin', function (event) {
+        event.target.setAttribute('data-value', event.target.innerText);
+      });
+    }
+  };
+  setupEdit('.edit-category', editedCategoryItem);
+  setupEdit('.edit-tag', editedTagItem);
 
   //  タグのカテゴリ選択編集
   const orgSelect = document.querySelector('#tmp-edit-catalog-tag-categories');
@@ -142,35 +125,36 @@ $(function () {
       }
       return newSelect;
     };
+    const setupSelect2 = ($newSelect, target, tag_id) => {
+      $newSelect.select2({
+        width: 'resolve',
+        placeholder: 'Multi-select',
+        closeOnSelect: false,
+        alloClear: true
+      });
+      $newSelect.select2('open');
+      $newSelect.on('select2:close', function (event) {
+        const values = Array.from(this.options).filter(x => x.selected).map(x => x.text).join(', ');
+        while (target.firstChild) { target.removeChild(target.firstChild); }
+        target.innerText = values;
+        if (target.getAttribute('data-value') === values) { return; }
+        // const tag_id = target.parentNode?.id?.slice(4); // 4='tag-'.length
+        const column = target.classList.item(0);
+        const value = values;
+        console.log(`tag ${tag_id} : ${column} : ${value}`);
+      });
+    };
     for (const edit of document.querySelectorAll('.edit2-tag')) {
       edit.addEventListener('click', function (event) {
         const target = this;
         const tmp = target.querySelector('.tmp-select');
         if (tmp) { return; }
         const tag_id = target.parentNode?.id?.slice(4); // 4='tag-'.length
-        const tmpSelect_id = `tmp-select-${tag_id}`;
         target.setAttribute('data-value', target.innerText);
-        const newSelect = makeSelect(target.innerText, tmpSelect_id);
+        const newSelect = makeSelect(target.innerText, `tmp-select-${tag_id}`);
         target.innerText = '';
         target.appendChild(newSelect);
-        const $newSelect = $(newSelect);
-        $newSelect.select2({
-          width: 'resolve',
-          placeholder: 'Multi-select',
-          closeOnSelect: false,
-          alloClear: true
-        });
-        $newSelect.select2('open');
-        $newSelect.on('select2:close', function (event) {
-          const values = Array.from(this.options).filter(x => x.selected).map(x => x.text).join(', ');
-          while (target.firstChild) { target.removeChild(target.firstChild); }
-          target.innerText = values;
-          if (target.getAttribute('data-value') === values) { return; }
-          // const tag_id = target.parentNode?.id?.slice(4); // 4='tag-'.length
-          const column = target.classList.item(0);
-          const value = values;
-          console.log(`tag ${tag_id} : ${column} : ${value}`);
-        });
+        setupSelect2($(newSelect), target, tag_id);
       });
     }
   }
