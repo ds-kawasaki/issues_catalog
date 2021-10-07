3eea4e47e71378bbb8fefcefed42284930bc975f
川﨑　澄明 <kawasaki@d-seeds.com>
2021-10-07 11:55:55 +0000

    jsコード整理

--- assets/javascripts/issues_catalog_settings.js
+++ assets/javascripts/issues_catalog_settings.js
@@ -58,55 +58,9 @@ $(function () {
     }
   };
 
-  //  タグカテゴリの項目編集
-  const editedCategoryItem = (target, value, oldValue) => {
-    const category_id = target.parentNode?.id?.slice(21); // 21='catalog_tag_category-'.length
-    const column = target.classList.item(0);
-    $.ajax({
-      type: 'PATCH',
-      url: '/catalog_tag_categories/field_update/',
-      data: `id=${category_id}&catalog_tag_category[${column}]=${value}`,
-    }).done(function () {
-      // console.log(`category changed: ${category_id} : ${column} : ${value}`);
-      if (column === 'name') { updateCategoryName(category_id, oldValue, value); }  // タグカテゴリ名称変更を各所の表示反映
-    }).fail(function (jqXHR, textStatus) {
-      console.log(`category change failed: ${category_id} : ${column} : ${value} : ${textStatus}`);
-      target.innerText = oldValue;  //  更新失敗したので元に戻す 
-    });
-  };
-  //  タググループの項目編集
-  const editedGroupItem = (target, value, oldValue) => {
-    const group_id = target.parentNode?.id?.slice(18); // 18='catalog_tag_group-'.length
-    const column = target.classList.item(0);
-    $.ajax({
-      type: 'PATCH',
-      url: '/catalog_tag_groups/field_update/',
-      data: `id=${group_id}&catalog_tag_group[${column}]=${value}`,
-    }).done(function () {
-      // console.log(`group changed: ${group_id} : ${column} : ${value}`);
-      if (column === 'name') { updateGroupName(group_id, oldValue, value); }  // タグカテゴリ名称変更を各所の表示反映
-    }).fail(function (jqXHR, textStatus) {
-      console.log(`group change failed: ${group_id} : ${column} : ${value} : ${textStatus}`);
-      target.innerText = oldValue;  //  更新失敗したので元に戻す 
-    });
-  };
-  //  タグの項目編集
-  const editedTagItem = (target, value, oldValue) => {
-    const tag_id = target.parentNode?.id?.slice(4); // 4='tag-'.length
-    const column = target.classList.item(0);
-    $.ajax({
-      type: 'PATCH',
-      url: '/catalog_tags/field_update/',
-      data: `id=${tag_id}&catalog_tag[${column}]=${value}`,
-    }).done(function () {
-      console.log(`tag changed ${tag_id} : ${column} : ${value}`);
-    }).fail(function (jqXHR, textStatus) {
-      console.log(`tag change failed: ${tag_id} : ${column} : ${value} : ${textStatus}`);
-      target.innerText = oldValue;  //  更新失敗したので元に戻す 
-    });
-  };
-  const setupEdit = (className, editedCallback) => {
-    const editedItem = (event, editedCallback) => {
+
+  const setupEdit = (className, idSliceNum, targetUrl, paramName, editedCallback) => {
+    const editedItem = (event) => {
       const target = event.target;
       target.innerText = target.innerText.replace(/[\r\n]/g, '').trim();  //  改行・冒頭末尾余白削除 
       const oldValue = target.getAttribute('data-value');
@@ -116,30 +70,43 @@ $(function () {
         return true;
       }
       event.preventDefault();
-      if (editedCallback) {
-        editedCallback(target, target.innerText || '__none__', oldValue);
-      }
+      const targetId = target.parentNode?.id?.slice(idSliceNum);
+      const column = target.classList.item(0);
+      const value = target.innerText || '__none__';
+      $.ajax({
+        type: 'PATCH',
+        url: targetUrl,
+        data: `id=${targetId}&${paramName}[${column}]=${value}`,
+      }).done(function () {
+        // console.log(`${paramName} changed: ${targetId} : ${column} : ${value}`);
+        if (column === 'name' && editedCallback) {
+          editedCallback(targetId, oldValue, value); // 名称変更を各所の表示反映
+        }
+      }).fail(function (jqXHR, textStatus) {
+        console.log(`${paramName} change failed: ${targetId} : ${column} : ${value} : ${textStatus}`);
+        target.innerText = oldValue;  //  更新失敗したので元に戻す 
+      });
       return false;
     };
     for (const edit of document.querySelectorAll(className)) {
       edit.contentEditable = true;
       edit.setAttribute('data-value', edit.innerText);
       edit.addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
+        if (event.key === 'Enter') {  //  改行させない 
           event.preventDefault();
         }
       });
       edit.addEventListener('focusout', function (event) {
-        editedItem(event, editedCallback);
+        editedItem(event);
       });
       edit.addEventListener('focusin', function (event) {
         event.target.setAttribute('data-value', event.target.innerText);
       });
     }
   };
-  setupEdit('.edit-category', editedCategoryItem);
-  setupEdit('.edit-group', editedGroupItem);
-  setupEdit('.edit-tag', editedTagItem);
+  setupEdit('.edit-category', 21, '/catalog_tag_categories/field_update/', 'catalog_tag_category', updateCategoryName); // 21='catalog_tag_category-'.length
+  setupEdit('.edit-group', 18, '/catalog_tag_groups/field_update/', 'catalog_tag_group', updateGroupName); // 18='catalog_tag_group-'.length
+  setupEdit('.edit-tag', 4, '/catalog_tags/field_update/', 'catalog_tag', null); // 4='tag-'.length
 
 
 
@@ -186,7 +153,7 @@ $(function () {
             data: data,
             dataType: 'json'
           }).done(function () {
-            console.log(`tag changed ${tag_id} : ${sendValues}`);
+            // console.log(`tag changed ${tag_id} : ${sendValues}`);
           }).fail(function (jqXHR, textStatus) {
             console.log(`tag change failed: ${tag_id} : ${sendValues} : ${textStatus}`);
             target.innerText = oldValue;  //  更新失敗したので元に戻す 
