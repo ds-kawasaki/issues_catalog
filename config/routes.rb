b52cf76836472ae346fa4baa8179a9090e1935ac
川﨑　澄明 <kawasaki@d-seeds.com>
2021-12-03 12:02:32 +0000

    コンテキストメニューでのタグ追加・削除処理削除 redmine_tagsに移植

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 get '/projects/:project_id/issues_catalog', to: 'issues_catalog#index', as: 'project_issues_catalog'
-
-get 'issues_catalog/add_tag', :to => 'issues_catalog#add_tag', :as => 'catalog_add_tag'
-get 'issues_catalog/delete_tag', :to => 'issues_catalog#delete_tag', :as => 'catalog_delete_tag'
-patch 'issues_catalog/update_tag', :to => 'issues_catalog#update_tag'
 patch 'catalog_tags/bulk_update', to: 'catalog_tags#bulk_update', as: 'catalog_tags_bulk_update'
 patch 'catalog_tags/field_update', to: 'catalog_tags#field_update'
 patch 'catalog_tag_categories/field_update', to: 'catalog_tag_categories#field_update'
