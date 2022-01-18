ff64683b54b8e64fd228d1ed0dd3ee6af97ac96a
川﨑　澄明 <kawasaki@d-seeds.com>
2022-01-18 11:08:56 +0000

    カタログ設定画面をタグをデフォルトに

--- lib/issues_catalog/patches/issues_catalog_projects_helper_patch.rb
+++ lib/issues_catalog/patches/issues_catalog_projects_helper_patch.rb
@@ -9,7 +9,7 @@ module IssuesCatalog
         action = { name: 'issues_catalog',
           action: :show,
           partial: 'issues_catalog_settings/show',
-          onclick: 'showTab(\'issues_catalog\', this.href); showTab(\'manage_tag_categories\', this.href)',
+          onclick: 'showTab(\'issues_catalog\', this.href); showTab(\'manage_tags\', this.href)',
           label: :issues_catalog }
         tabs << action if @project.module_enabled?(:issues_catalog)
 
