b9783f0aee49454b95da4c32fdaf8dc233a59d19
川﨑　澄明 <kawasaki@d-seeds.com>
2022-05-16 11:21:16 +0000

    callbackFocusの引数をeventからelemに変更

--- front_src/javascripts/modules/editTableBase.js
+++ front_src/javascripts/modules/editTableBase.js
@@ -19,7 +19,7 @@ export class EditTableBase {
     EditTableBase.#callbackBlurNowTarget = null;
     EditTableBase.#editTypes = new Map();
 
-    document.addEventListener('click', (event) => EditTableBase.#clicked(event));
+    document.addEventListener('click', (event) => EditTableBase.#clicked(event.target));
   }
 
   static registEdit(className, callbackFocus, callbackBlur) {
@@ -64,28 +64,27 @@ export class EditTableBase {
 
 
 
-  static #clicked(event) {
-    const target = event.target;
+  static #clicked(target) {
     if (target === EditTableBase.#nowTarget) {
       // console.log(`click(same): ${target.className}`);
     } else {
       const callbacks = EditTableBase.#canEdit(target);
       if (callbacks) {
+        // console.log(`click(newTarget): ${target?.className}`);
         if (EditTableBase.#callbackBlurNowTarget && EditTableBase.#nowTarget) {
           EditTableBase.#callbackBlurNowTarget(EditTableBase.#nowTarget);
         }
-        // console.log(`click(newTarget): ${target.className}`);
         if (callbacks.callbackFocus) {
-          callbacks.callbackFocus(event);
+          callbacks.callbackFocus(target);
         }
         EditTableBase.#nowTarget = target;
         EditTableBase.#callbackBlurNowTarget = callbacks.callbackBlur;
       } else if (EditTableBase.#isSosenNowTarget(target)) {
-        // console.log(`click(sosenNowTarget): ${target.className}`);
+        // console.log(`click(sosenNowTarget): ${target?.className}`);
       } else if (EditTableBase.#isSosenSelect2Open(target)) {  //  Select2のドロップダウン中はbodyの最後になるっぽいので、それは無視する 
-        // console.log(`click(sosenSelect2Open): ${target.className}`);
+        // console.log(`click(sosenSelect2Open): ${target?.className}`);
       } else {
-        // console.log(`click(other): ${target.className}`);
+        // console.log(`click(other): ${target?.className} : ${EditTableBase.#nowTarget}`);
         if (EditTableBase.#callbackBlurNowTarget && EditTableBase.#nowTarget) {
           EditTableBase.#callbackBlurNowTarget(EditTableBase.#nowTarget);
         }
@@ -96,6 +95,7 @@ export class EditTableBase {
   }
 
   static #canEdit(target) {
+    if (target === null) { return null; }
     let ret = null;
     let callbacks = EditTableBase.#editTypes.get(target.className);
     if (callbacks !== undefined) {
