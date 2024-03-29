dd6948611db6b6e898da7e949460b9489a29f70b
Sumiaki Kawasaki <kawasaki@d-seeds.com>
2024-03-29 19:59:07 +0900

    バージョン番号1.1.6に

--- init.rb
+++ init.rb
@@ -12,7 +12,7 @@ Redmine::Plugin.register :issues_catalog do
   name 'Issues Catalog plugin'
   author 'ds-kawasaki'
   description 'issues catalog plugin for Redmine'
-  version '1.1.5'
+  version '1.1.6'
   url 'https://github.com/ds-kawasaki/issues_catalog'
   author_url 'https://github.com/ds-kawasaki'
 
